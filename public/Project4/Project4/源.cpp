0e45ce277f0de175a313016bdcf1abc17078c2ca
ningmengqixi <89135503+ningmengqixi@users.noreply.github.com>
2021-09-18 17:07:27 +0800

    Add files via upload

---
+++ public/Project4/Project4/源.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//int main()
+//{
+//	printf("\t\t\t*\n");
+//	printf("\t\t*\t\t*\n");
+//	printf("\t*\t\t\t\t*\n");
+//	printf("\t\t*\t\t*\n");
+//	printf("\t\t\t*\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if (age < 60)
+//		printf("成年");
+//	else
+//	printf("老年");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	
+//	
+//	while (100>a)
+//	{
+//		if(a%b==1)
+//		printf("%d\n", a);
+//		a++;
+//	}
+int main()
+{
+	int a = 0;
+	scanf("%d", &a);
+	switch (a)
+	{
+		case 1
+			printf("星期一");
+			break;
+	}
+
+	
+
+	
+	
+	
+	
+	return 0;
+}
